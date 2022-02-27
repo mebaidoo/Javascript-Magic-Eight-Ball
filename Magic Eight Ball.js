0606a84b77758972ebd28d9e958112303d9124ea
Maame Esi Baidoo <87780959+mebaidoo@users.noreply.github.com>
2022-02-27 23:28:30 +0000

    Add files via upload

---
+++ Magic Eight Ball.js
@@ -0,0 +1,15 @@
+//Converting years to dog years
+//Creating a variable myAge that will be constant
+const myAge = 24;
+//Creating a variable earlyYears
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears, laterYears);
+//Calculating the total dog years
+var myAgeInDogYears = earlyYears + laterYears;
+//Saving my name to a variable in lower case
+var myName = 'Esi'.toLowerCase();
+//Displaying name and age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
